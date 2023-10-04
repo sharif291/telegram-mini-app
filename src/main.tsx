e51a53703c0a1fa2cf5b98ba414d1c31c6cd79ab
Sharif Hossain <sharif.hossain.iubat@gmail.com>
2023-10-04 23:37:16 +0600

    ton wallet added

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { TonConnectUIProvider } from "@tonconnect/ui-react";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+// this manifest is used temporarily for development purposes
+const manifestUrl =
+  "https://raw.githubusercontent.com/ton-community/tutorials/main/03-client/test/public/tonconnect-manifest.json";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <TonConnectUIProvider manifestUrl={manifestUrl}>
+      <App />
+    </TonConnectUIProvider>
+  </React.StrictMode>
+);
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,18 +1,45 @@
-import {  useState } from "react";
+import { useState } from "react";
 import PairSelection from "../components/PairSelection";
 import AmountSelection from "../components/AmountSelection";
 import DirectionSelection from "../components/DirectionSelection";
 import PredictionComponent from "../components/PredictionComponent";
 import PrimaryButton from "../components/Button/PrimaryButton";
 
+import { TonConnectButton, useTonConnectUI } from "@tonconnect/ui-react";
+
 function home() {
+  const [tonConnectUI, setOptions] = useTonConnectUI();
+
   const [loading] = useState(false);
   const [runPrediction, setRunPrediction] = useState(false);
   const toogleRunPrediction = () => {
     setRunPrediction(!runPrediction);
   };
+
+  const myTransaction = {
+    validUntil: Math.floor(Date.now() / 1000) + 60, // 60 sec
+    messages: [
+      {
+        address: "EQCJnYQhJs8Siz5Q4H-7ghHdNChcfcwofhv8ni1zb-xq9MK7",
+        amount: "11111",
+      },
+    ],
+  };
+
   return (
     <div className="max-w-[400px] w-[300px] mt-6 p-2">
+      <div className=" m-auto">
+        <TonConnectButton style={{ margin: "auto" }} />
+      </div>
+
+      <PrimaryButton
+        text={"Send transaction"}
+        isDisble={false}
+        buttonHandler={() => {
+          tonConnectUI.sendTransaction(myTransaction);
+        }}
+      ></PrimaryButton>
+
       {runPrediction ? (
         <PredictionComponent
           toogleRunPrediction={toogleRunPrediction}
